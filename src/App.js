4aa0d8e68aef599e5917642501f2c1fe06985f4c
Lautaro Scarafia <lautiisca17@gmail.com>
2023-12-05 23:40:09 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./components/Inicio";
+import CrearEncuesta from "./components/CrearEncuesta";
+import Encuesta from "./components/Encuesta";
+import Menu from "./components/Menu";
+import NotFound from "./components/NotFound";
+import encuestas from "./data/encuestas.json";
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
+
+  const [listaEncuestas, setListaEncuestas] = useState(encuestas);
+const agregarEncuesta = (nuevaEncuesta) => {nuevaEncuesta.id = listaEncuestas.length + 1
+setListaEncuestas([...listaEncuestas, nuevaEncuesta]);
+};
+const responderEncuesta = (id, respuestas) => {
+const encuesta = listaEncuestas.find(enc => enc.id ===
+parseInt(id));
+encuesta.respuestas = [respuestas];
+};
+
+
+
+
+
   return (
-    <div className="App">
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ejercicio 5 - Encuestas en React v2</h1>
       </header>
+      <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Inicio listaEncuestas={listaEncuestas} />} />
+        <Route
+          path="/encuesta/crear"
+          element={<CrearEncuesta agregarEncuesta={agregarEncuesta} />}
+        />
+        <Route
+          path="/encuesta/:id"
+          element={
+            <Encuesta
+              listaEncuestas={listaEncuestas}
+              responderEncuesta={responderEncuesta}
+            />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+      <footer className="App-footer">
+        <p>
+          Página creada para el Ejercicio 5 de Curso Frontend de React 2023 -
+          Ing. Rubén Romero | Alumno: Lautaro Scarafia
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+
+
+const NotFound = () => (
+    <div>
+      <h2 className="e404h2">404 - Not Found!</h2>
+      <p>No encontramos la página que estás buscando.</p>
+      <br />
+    </div>
+  );
+  export default NotFound;
